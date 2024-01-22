594248c3a63cbf55b8ab7769255e6508d83269b4
Majid Moradi <majidmoradi1368@yahoo.com>
2024-01-22 12:04:26 +0330

    Products models added.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self',verbose_name='parent'
+                               ,blank=True,null=True
+                               ,on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=50)
+    description = models.TextField(_('description'),blank=True)
+    avatar = models.ImageField(blank=True,upload_to='categories/ ')
+    is_enable = models.BooleanField(default=True)
+    creates_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+class Product(models.Model):
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(blank=True, upload_to='products/')
+    is_enable = models.BooleanField(default=True)
+    categories = models.ManyToManyField('Category',verbose_name='categories',blank=True )
+    creates_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
+class File(models.Model):
+    product = models.ForeignKey('Product',verbose_name='product',on_delete=models.CASCADE)
+    title = models.CharField(_('title'), max_length=50)
+    file = models.FileField('file',upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(default=True)
+    creates_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
+
